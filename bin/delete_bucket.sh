3ab39c4c98fefb95cf123dab2da0de033c740df3
ryanjohnson <ryan@shakainteractive.com>
2017-12-12 21:38:50 -0500

    add shell scripts

---
+++ bin/delete_bucket.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eu
+
+STAGE="${1:-dev}"
+
+echo "DELETE ALL STATIC ASSETS from ${STAGE}"
+
+echo "Deleting static assets from ${STAGE}..."
+
+BUCKET_NAME=$(aws \
+    cloudformation describe-stacks \
+    --stack-name "ryanjohnsondotca-${STAGE}" \
+    --query "Stacks[0].Outputs[?OutputKey=='WebSiteBucket'] | [0].OutputValue" \
+    --output text \
+    --profile ryanjohnson.ca-devops)
+
+mkdir /tmp/empty
+
+aws s3 sync --delete /tmp/empty/ "s3://${BUCKET_NAME}/"
+
+rmdir /tmp/empty
+
+echo "Bucket ${BUCKET_NAME} has been emptied"
